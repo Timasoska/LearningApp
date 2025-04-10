8fa77a8d5fb281badb63dd0be1dab137942792c8
Timofey <Tim005rud@gmail.com>
2025-04-10 16:47:56 +0300

    Мой ЮИ

--- app/src/main/java/com/example/learningapp/presentation/ui/MainScreen.kt
+++ app/src/main/java/com/example/learningapp/presentation/ui/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.example.learningapp.presentation.ui
 
+import android.graphics.drawable.shapes.Shape
+import android.security.identity.AlreadyPersonalizedException
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +11,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -19,15 +23,22 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.learningapp.domain.model.Subject
 import com.example.learningapp.presentation.subject.SubjectIntent
@@ -36,6 +47,7 @@ import com.example.learningapp.presentation.subject.SubjectViewModel
 @Composable
 fun MainScreen(viewmodel: SubjectViewModel){
     val state by viewmodel.state.collectAsState()
+    var textOfSubject = remember{ mutableStateOf("")}
 
     LaunchedEffect(Unit) {
         viewmodel.processIntent(SubjectIntent.LoadSubject)
@@ -43,7 +55,7 @@ fun MainScreen(viewmodel: SubjectViewModel){
 
     LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         items(20) {
-            SubjectItem()
+            SubjectItem("Subject")
         }
     }
     Row(modifier = Modifier.fillMaxSize().padding(16.dp),
@@ -51,8 +63,9 @@ fun MainScreen(viewmodel: SubjectViewModel){
         verticalAlignment = Alignment.Bottom
     ) {
         Button(
+            shape = CircleShape,
             onClick = {},
-            //modifier = Modifier.size(50.dp),
+            modifier = Modifier.size(75.dp),
         ) {
             Icon(Icons.Default.Add, "")
         }
@@ -61,17 +74,20 @@ fun MainScreen(viewmodel: SubjectViewModel){
 }
 
 @Composable
-fun SubjectItem(){
+fun SubjectItem(textOfSubject: String){
 
     ElevatedCard(modifier = Modifier
-        .padding(16.dp)
+        .padding(vertical = 16.dp)
         .fillMaxWidth(1f)
         .height(100.dp),
         onClick = {},
         elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
     ) {
-        Text(text = "eee",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+        Text(text = textOfSubject,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(25.dp),
+            fontSize = 28.sp
         )
     }
 }
